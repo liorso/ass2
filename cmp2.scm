db584a99f217013b7475a92cc95d6017ba0a4dbe
liorso <liorso@gmail.com>
2016-12-04 10:36:54 -0800

    Mayer's code + var impl

--- cmp2.scm
+++ cmp2.scm
@@ -1,2 +1,185 @@
-starting and continue
-ad
+;;; qq.scm
+;;; A naive, one-level quasiquote implementation + optimizations
+;;;
+;;; Programmer: Mayer Goldberg, 2016
+
+(load "pattern-matcher.scm")
+
+;;;
+
+(define ^quote?
+  (lambda (tag)
+    (lambda (e)
+      (and (pair? e)
+	   (eq? (car e) tag)
+	   (pair? (cdr e))
+	   (null? (cddr e))))))
+
+(define quote? (^quote? 'quote))
+(define unquote? (^quote? 'unquote))
+(define unquote-splicing? (^quote? 'unquote-splicing))
+
+(define const?
+  (let ((simple-sexprs-predicates
+	 (list boolean? char? number? string?)))
+    (lambda (e)
+      (or (ormap (lambda (p?) (p? e))
+		 simple-sexprs-predicates)
+	  (quote? e)))))
+
+(define quotify
+  (lambda (e)
+    (if (or (null? e)
+	    (pair? e)
+	    (symbol? e)
+	    (vector? e))
+	`',e
+	e)))
+
+(define unquotify
+  (lambda (e)
+    (if (quote? e)
+	(cadr e)
+	e)))
+
+(define const-pair?
+  (lambda (e)
+    (and (quote? e)
+	 (pair? (cadr e)))))
+
+(define expand-qq
+  (letrec ((expand-qq
+	    (lambda (e)
+	      (cond ((unquote? e) (cadr e))
+		    ((unquote-splicing? e)
+		     (error 'expand-qq
+		       "unquote-splicing here makes no sense!"))
+		    ((pair? e)
+		     (let ((a (car e))
+			   (b (cdr e)))
+		       (cond ((unquote-splicing? a)
+			      `(append ,(cadr a) ,(expand-qq b)))
+			     ((unquote-splicing? b)
+			      `(cons ,(expand-qq a) ,(cadr b)))
+			     (else `(cons ,(expand-qq a) ,(expand-qq b))))))
+		    ((vector? e) `(list->vector ,(expand-qq (vector->list e))))
+		    ((or (null? e) (symbol? e)) `',e)
+		    (else e))))
+	   (optimize-qq-expansion (lambda (e) (optimizer e (lambda () e))))
+	   (optimizer
+	    (compose-patterns
+	     (pattern-rule
+	      `(append ,(? 'e) '())
+	      (lambda (e) (optimize-qq-expansion e)))
+	     (pattern-rule
+	      `(append ,(? 'c1 const-pair?) (cons ,(? 'c2 const?) ,(? 'e)))
+	      (lambda (c1 c2 e)
+		(let ((c (quotify `(,@(unquotify c1) ,(unquotify c2))))
+		      (e (optimize-qq-expansion e)))
+		  (optimize-qq-expansion `(append ,c ,e)))))
+	     (pattern-rule
+	      `(append ,(? 'c1 const-pair?) ,(? 'c2 const-pair?))
+	      (lambda (c1 c2)
+		(let ((c (quotify (append (unquotify c1) (unquotify c2)))))
+		  c)))
+	     (pattern-rule
+	      `(append ,(? 'e1) ,(? 'e2))
+	      (lambda (e1 e2)
+		(let ((e1 (optimize-qq-expansion e1))
+		      (e2 (optimize-qq-expansion e2)))
+		  `(append ,e1 ,e2))))
+	     (pattern-rule
+	      `(cons ,(? 'c1 const?) (cons ,(? 'c2 const?) ,(? 'e)))
+	      (lambda (c1 c2 e)
+		(let ((c (quotify (list (unquotify c1) (unquotify c2))))
+		      (e (optimize-qq-expansion e)))
+		  (optimize-qq-expansion `(append ,c ,e)))))
+	     (pattern-rule
+	      `(cons ,(? 'e1) ,(? 'e2))
+	      (lambda (e1 e2)
+		(let ((e1 (optimize-qq-expansion e1))
+		      (e2 (optimize-qq-expansion e2)))
+		  (if (and (const? e1) (const? e2))
+		      (quotify (cons (unquotify e1) (unquotify e2)))
+		      `(cons ,e1 ,e2))))))))
+    (lambda (e)
+      (optimize-qq-expansion
+       (expand-qq e)))))
+
+
+
+;------------------------------LIOR------------------------------------------------------------------------
+
+;-------var----
+(define *reserved-words*
+  '(and begin cond define do else if lambda
+        let let* letrec or quasiquote unquote
+        unquote-splicing quote set!))
+
+(define reserved-word?
+  (lambda (v) (ormap (lambda (x) (equal? v x)) *reserved-words*)
+    ))
+
+(define var?
+  (lambda (v)
+    (and (symbol? v) (not (reserved-word? v)))
+    ))
+
+
+
+
+
+;--------------------------------From guide 2015 --------------------------------------------------------------------
+
+
+
+
+
+
+
+(define parse
+  (let ((run
+	 (compose-patterns
+          ;---------------------const---------------not impl 
+	  (pattern-rule
+	   (? 'c simple-const?)
+	   (lambda (c) `(const ,c)))
+
+          ;---------------------quote---------------not impl
+	  (pattern-rule
+	   `(quote ,(? 'c))
+	   (lambda (c) `(const ,c)))
+
+          ;---------------------var-----------------implimented
+	  (pattern-rule
+	   (? 'v var?)
+	   (lambda (v) `(var ,v)))
+
+          ;---------------------if-----------------not impl
+	  (pattern-rule
+	   `(if ,(? 'test) ,(? 'dit))
+	   (lambda (test dit)
+	     `(if ,(parse test) ,(parse dit) (const ,*void-object*))))
+	  (pattern-rule
+	   `(if ,(? 'test) ,(? 'dit) ,(? 'dif))
+	   (lambda (test dit dif)
+	     `(if ,(parse test) ,(parse dit) ,(parse dif))))
+
+          ;---------------------let*-----------------not impl
+	  ;; let*
+	  (pattern-rule
+	   `(let* () ,(? 'expr) . ,(? 'exprs list?))
+	   (lambda (expr exprs)
+	     (parse (beginify (cons expr exprs)))))
+	  (pattern-rule
+	   `(let* ((,(? 'var var?) ,(? 'val)) . ,(? 'rest)) . ,(? 'exprs))
+	   (lambda (var val rest exprs)
+	     (parse `(let ((,var ,val))
+		       (let* ,rest . ,exprs)))))
+	  ;; add more rules here
+	  )))
+    (lambda (e)
+      (run e
+	   (lambda ()
+	     (error 'parse
+		    (format "isn't implemented yet" e)))))))
