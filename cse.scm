654445d318f1dd3ea12bbee6b2c44eb72667fdd7
peerd77 <peerd77@gmail.com>
2016-12-15 18:13:45 +0200

    cse.scm added because accedently deleted and forgot to add in last commit

---
+++ cse.scm
@@ -0,0 +1,89 @@
+(load "pattern-matcher.scm")
+
+
+;--------------------------finding
+(define quotedList?
+  (let ((run
+	 (compose-patterns
+	  (pattern-rule
+	   `(quote ,(? 'c))
+	   (lambda (c) `(const ,c)))         
+	  )))
+    (lambda (e)
+      (run e
+	   (lambda ()
+	     #f)))))
+
+(define real-help-find-recuuring
+  (lambda (first next)
+    (if (or (quotedList? first) (not (pair? first)) (not (pair? next)) (null? next)) #f
+        (if (and (equal? first (car next))) first
+            (or
+             (if (pair? (car next)) (real-help-find-recuuring first (car next)) #f)
+             (real-help-find-recuuring first (cdr next))
+             (ormap (lambda (deep-first) (real-help-find-recuuring deep-first next)) first)
+             )))
+    ))
+
+(define help-find-recuuring
+  (lambda (first next)
+    (if (real-help-find-recuuring first next) (real-help-find-recuuring first next) '())))
+;(define help-find-recuuring
+;  (lambda (first next)
+;    (if (or (quotedList? first) (not (pair? first)) (not (pair? next)) (null? next)) '()
+;        (if (and (equal? first (car next))) first 
+;            (or
+;             (if (pair? (car next)) (help-find-recuuring first (car next)) #f)
+;             (if (null? (help-find-recuuring first (cdr next))) #f (help-find-recuuring first (cdr next)))
+;             (map (lambda (deep-first) (help-find-recuuring deep-first next)) first)             
+;             )))
+;    ))
+
+;(define find-recurring
+;  (lambda (e)
+;    (begin (define recurring (help-find-recuuring (car e) (cdr e)))
+;           (if recurring recurring
+;               (if (null? (cddr e)) #f
+;                   (find-recurring (cdr e)))))  
+;    ))
+(define delet-null
+  (lambda (e) (display e)
+    (if (null? e) e
+        (if (null? (car e)) (delet-null (cdr e))
+            (append (car e) (delet-null (cdr e)))))
+    ))
+
+(define find-recurring
+  (lambda (e)
+    (if (null? e) '()
+         (append (if (null? (help-find-recuuring (car e) (cdr e))) '()
+                            `(,(help-find-recuuring (car e) (cdr e))))
+                     (find-recurring (cdr e))))                     
+    ))
+
+
+;-------------------Handaling founded
+(define change-rec
+  (lambda (e recurring new-sym)
+    (if (or (not (pair? e)) (null? e)) (list)
+        (if (equal? recurring (car e)) `(,new-sym ,@(change-rec (cdr e) recurring new-sym))
+            (if (pair? (car e)) `(,(change-rec (car e) recurring new-sym) ,@(change-rec (cdr e) recurring new-sym))
+                `(,(car e) ,@(change-rec (cdr e) recurring new-sym)))))
+    ))
+        
+
+(define compose-let
+  (lambda (e recurring)
+    (begin (define new-sym (gensym)) ; Maybe problematic to use "new-sym"
+           `(let* ([,new-sym ,recurring]) ,(change-rec e recurring new-sym)))
+    ))
+
+
+;------------------------------main
+(define cse
+  (lambda (e)
+    (begin (define recurring (find-recurring e))
+           (map find-recurring recurring)
+           (if recurring (cse (compose-let e recurring));todo
+               e))
+    ))
