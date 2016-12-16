707ba559cc5c5c210dc709223daeb2e9bbcfdd3c
lioso <liorso@gmail.com>
2016-12-16 09:28:11 -0800

    final cse! need to test Asaf

--- cse.scm
+++ cse.scm
@@ -32,30 +32,6 @@
                                                       ,@(help-find-recuuring first (cdr next))) 
                                                       (help-find-recuuring first (cdr next))))
     ))
-;(define help-find-recuuring
-;  (lambda (first next)
-;    (if (or (quotedList? first) (not (pair? first)) (not (pair? next)) (null? next)) '()
-;        (if (and (equal? first (car next))) first 
-;            (or
-;             (if (pair? (car next)) (help-find-recuuring first (car next)) #f)
-;             (if (null? (help-find-recuuring first (cdr next))) #f (help-find-recuuring first (cdr next)))
-;             (map (lambda (deep-first) (help-find-recuuring deep-first next)) first)             
-;             )))
-;    ))
-
-;(define find-recurring
-;  (lambda (e)
-;    (begin (define recurring (help-find-recuuring (car e) (cdr e)))
-;           (if recurring recurring
-;               (if (null? (cddr e)) #f
-;                   (find-recurring (cdr e)))))  
-;    ))
-(define delet-null
-  (lambda (e)
-    (if (null? e) e
-        (if (null? (car e)) (delet-null (cdr e))
-            (append (car e) (delet-null (cdr e)))))
-    ))
 
 (define find-recurring
   (lambda (e)
@@ -65,32 +41,31 @@
     ))
 
 
-;-------------------Handaling founded
+;-------------------making let
 (define make-list-for-let
-	(lambda (recurring syms)
-		(if (null? syms) (list)
-		`([,(car syms) ,(car recurring)] ,(make-list-for-let (cdr recurring) (cdr syms))))
+	(lambda (syms recurring)
+		(if (null? (cdr syms)) `([,(car syms) ,(car recurring)])
+                    `([,(car syms) ,(car recurring)] ,@(make-list-for-let (cdr syms) (cdr recurring))))
 		))
-		
+
+(define real-change-body
+  (lambda (sym recurring e)
+    (cond
+      ((not (pair? e)) e)
+      ((equal? e recurring) sym)
+      ((null? (cdr e)) `(,(real-change-body sym recurring (car e))))
+      (else `(,(real-change-body sym recurring (car e)) ,@(real-change-body sym recurring (cdr e)))))
+    ))
+                                                            
+                         
+
 (define change-body
-	(lambda (e recurring syms)
-		(if (null? e) '())))
-        
-
-(define compose-let
-  (lambda (e recurring syms)
-           `(let* ,(make-list-for-let recurring syms) ,(change-body e recurring syms)))
-    )
-
+	(lambda (syms recurring e)
+		(if (null? (cdr syms)) (real-change-body (car syms) (car recurring) e)
+                    (change-body (cdr syms) (cdr recurring) (real-change-body (car syms) (car recurring) e)))
+          ))
 
 ;------------------------------prepare the recurring
-(define change-rec
-  (lambda (e recurring new-sym)
-    (if (or (not (pair? e)) (null? e)) (list)
-        (if (equal? recurring (car e)) `(,new-sym ,@(change-rec (cdr e) recurring new-sym))
-            (if (pair? (car e)) `(,(change-rec (car e) recurring new-sym) ,@(change-rec (cdr e) recurring new-sym))
-                `(,(car e) ,@(change-rec (cdr e) recurring new-sym)))))
-    ))
 	
 (define remove-dupe
   (lambda (l)
@@ -98,49 +73,6 @@
         (cons (car l) (remove-dupe (filter (lambda (x) (not (equal? x (car l)))) 
                                     (cdr l)))))
     ))
-
-(define reverseList
-  (lambda (l)
-    (if (null? l) '()
-     (append (reverseList (cdr l)) (list (car l))))
-    ))
-	
-;(define depend ;TODO: test
-;	(lambda (first next)
-;		(if (or (not (pair? next)) (null? next)) #t
-;			(if (equal? fisrt (car next)) #f
-;				(and (depend first (cdr next))
-;					(depend first (car next)))))
-;					))
-			
-
-;(define order ;TODO: test
-;	(lambda (li n)
-;		(if (null? li) (list)
-;			(if (and (= n 0)) (exeption) ; TODO
-;				(if (depend (car li) (cdr li)) (order (list (cdr li) (car li)) (- n 1))
-;					(list (car li) (order (cdr li) (length (cdr li)))))))))
-
-
-;(define change-instance-in-curring
- ; (lambda (next first sym)
-  ;  (cond ((null? next) '())
-   ;       ((not (pair? next)) next)
-    ;      ((equal? (car next) first) `(,sym ,(change-instance-in-curring (cdr next) first sym)))
-     ;     ((member first (list (car next))) `(,`(,(change-instance-in-curring (car next) first sym))
-      ;                                        ,(change-instance-in-curring (cdr next) first sym)))
-       ;   (else (change-instance-in-curring (cdr next) first sym)))
-;))
-			
-			
-;(define cange-in-curring
-;	(lambda (syms first next)
-;		(if (null? next) `(,first ,(car syms))
- ;                   (begin (set! canged (change-instance-in-curring next first (car syms)))
-;			(if (null? canged) `(,first ,(car syms))
- ;                           `((,first ,(car syms)) ,(cange-in-curring (cdr syms) (car canged) (cdr canged))))))
-  ;        ))
-                                                        
 
 
 (define deeplength
@@ -174,7 +106,7 @@
 
 (define real-change-in-curring
   (lambda (sym first next-with-sym)
-    (if (null? next-with-sym) (list);(cdr next-with-sym)) `(,(caar next-with-sym) ,(find-replace sym first (cadar next-with-sym)))
+    (if (null? next-with-sym) (list)
                `(,`(,(caar next-with-sym) ,(find-replace sym first (cadar next-with-sym)))
                                          ,@(real-change-in-curring sym first (cdr next-with-sym))))
     ))
@@ -199,7 +131,13 @@
                                         (set! recurring-with-syms 
                                                                    (make-list-syms-curring syms recurring-right-order))
 					(set! recurring-with-syms-changed (cange-in-curring syms recurring-with-syms))
-recurring-with-syms-changed
-                                        ;(compose-let e recurring-with-syms syms))
-               ))
-    )))
+                                        `(,(if (= 1 (length syms)) 'let 'let*) 
+                                          ,(make-list-for-let
+                                            (map (lambda (x) (if (pair? x) (car x) x)) recurring-with-syms-changed) 
+                                            (map (lambda (x) (if (pair? x) (cadr x) x)) recurring-with-syms-changed))
+                                          ,(change-body
+                                            (map (lambda (x) (if (pair? x) (car x) x)) recurring-with-syms-changed) 
+                                            (map (lambda (x) (if (pair? x) (cadr x) x)) recurring-with-syms-changed)
+                                            e)))
+                         ))
+    ))
