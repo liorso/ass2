04bb7ac46eae129d5a26ea0bf0e45998a774e68a
lioso <liorso@gmail.com>
2016-12-16 08:05:10 -0800

    todo: compose the final let

--- cse.scm
+++ cse.scm
@@ -1,5 +1,5 @@
 (load "pattern-matcher.scm")
-
+(print-gensym #f)
 
 ;--------------------------finding
 (define quotedList?
@@ -17,7 +17,7 @@
 (define real-help-find-recuuring
   (lambda (first next)
     (if (or (quotedList? first) (not (pair? first)) (not (pair? next)) (null? next)) #f
-        (if (equal? first (car next)) first
+        (if (or (equal? first next) (equal? first (car next))) first
             (or
              (real-help-find-recuuring first (cdr next))
              (if (pair? (car next)) (real-help-find-recuuring first (car next)) #f)        
@@ -27,7 +27,11 @@
 
 (define help-find-recuuring
   (lambda (first next)
-    (if (real-help-find-recuuring first next) (real-help-find-recuuring first next) '())))
+    (if (null? next) '()
+        (if (real-help-find-recuuring first (car next)) `(,(real-help-find-recuuring first (car next)) 
+                                                      ,@(help-find-recuuring first (cdr next))) 
+                                                      (help-find-recuuring first (cdr next))))
+    ))
 ;(define help-find-recuuring
 ;  (lambda (first next)
 ;    (if (or (quotedList? first) (not (pair? first)) (not (pair? next)) (null? next)) '()
@@ -47,7 +51,7 @@
 ;                   (find-recurring (cdr e)))))  
 ;    ))
 (define delet-null
-  (lambda (e) (display e)
+  (lambda (e)
     (if (null? e) e
         (if (null? (car e)) (delet-null (cdr e))
             (append (car e) (delet-null (cdr e)))))
@@ -56,8 +60,7 @@
 (define find-recurring
   (lambda (e)
     (if (null? e) '()
-         (append (if (null? (help-find-recuuring (car e) (cdr e))) '()
-                            `(,(help-find-recuuring (car e) (cdr e))))
+         (append (help-find-recuuring (car e) (cdr e))
                      (find-recurring (cdr e))))                     
     ))
 
@@ -119,19 +122,26 @@
 ;					(list (car li) (order (cdr li) (length (cdr li)))))))))
 
 
-;(define help-cange-in-curring ;TODO: test;
-;	(lambda (syms fisrt next)
-;		(if (or (not (pair? next)) (null? li)) (list)
-;			(if (equal? first (car next)) (list )))))
+;(define change-instance-in-curring
+ ; (lambda (next first sym)
+  ;  (cond ((null? next) '())
+   ;       ((not (pair? next)) next)
+    ;      ((equal? (car next) first) `(,sym ,(change-instance-in-curring (cdr next) first sym)))
+     ;     ((member first (list (car next))) `(,`(,(change-instance-in-curring (car next) first sym))
+      ;                                        ,(change-instance-in-curring (cdr next) first sym)))
+       ;   (else (change-instance-in-curring (cdr next) first sym)))
+;))
 			
 			
-(define cange-in-curring
-	(lambda (syms first next)
-		(if (null? next) (list)
-			(list first (change-in-curring (cdr syms) 
-							(car (cahge-recurring next first (car syms))) 
-                                                        (cdr (cahge-recurring next first (car syms))))))
-		))
+;(define cange-in-curring
+;	(lambda (syms first next)
+;		(if (null? next) `(,first ,(car syms))
+ ;                   (begin (set! canged (change-instance-in-curring next first (car syms)))
+;			(if (null? canged) `(,first ,(car syms))
+ ;                           `((,first ,(car syms)) ,(cange-in-curring (cdr syms) (car canged) (cdr canged))))))
+  ;        ))
+                                                        
+
 
 (define deeplength
   (lambda (x)
@@ -139,6 +149,45 @@
           ((pair? (car x)) (+ (deeplength (car x)) (deeplength (cdr x))))
           (else (+ 1 (deeplength (cdr x)))))
     ))
+
+(define rec-make-list-syms-curring
+  (lambda (syms curring lst)
+    (if (null? syms) lst
+        (rec-make-list-syms-curring (cdr syms) (cdr curring) `(,@lst ,`(,(car syms) ,(car curring)))))
+    ))
+
+(define make-list-syms-curring
+  (lambda (syms curring)
+        (rec-make-list-syms-curring (cdr syms) (cdr curring) `(,`(,(car syms) ,(car curring))))
+    ))
+
+(define find-replace
+  (lambda (sym first e)
+    (cond
+      ((not (pair? e)) e)
+      ((equal? first e) sym)
+      ((member first e) `(,(find-replace sym first (car e)) ,@(find-replace sym first (cdr e))))
+      ((null? (cdr e)) (list (find-replace sym first (car e))))
+      (else e))
+    ))
+           
+
+(define real-change-in-curring
+  (lambda (sym first next-with-sym)
+    (if (null? next-with-sym) (list);(cdr next-with-sym)) `(,(caar next-with-sym) ,(find-replace sym first (cadar next-with-sym)))
+               `(,`(,(caar next-with-sym) ,(find-replace sym first (cadar next-with-sym)))
+                                         ,@(real-change-in-curring sym first (cdr next-with-sym))))
+    ))
+
+
+(define cange-in-curring
+  (lambda (syms curring-list-with-sym)
+    (if (null? (cdr syms)) curring-list-with-sym
+        `(,(car curring-list-with-sym) ,@(cange-in-curring (cdr syms)
+                                                 (real-change-in-curring (car syms) 
+                                                                         (cadar curring-list-with-sym) 
+                                                                         (cdr curring-list-with-sym))))
+        )))
 		
 ;--------------------------------------main
 (define cse
@@ -146,10 +195,11 @@
     (begin (define recurring (remove-dupe (find-recurring e)))
 			(if (null? recurring) e	
 			 (begin (set! recurring-right-order (sort (lambda (x y) (< (deeplength x) (deeplength y))) recurring))					
-					(set! syms (map (lambda (x) (gensym)) (car recurring-right-order)))
-					(set! recurring-with-syms (cange-in-curring syms (car recurring-right-order)
-                                                                                    (car recurring-right-order)))
-recurring-with-syms
+					(set! syms (map (lambda (x) (gensym)) recurring-right-order))
+                                        (set! recurring-with-syms 
+                                                                   (make-list-syms-curring syms recurring-right-order))
+					(set! recurring-with-syms-changed (cange-in-curring syms recurring-with-syms))
+recurring-with-syms-changed
                                         ;(compose-let e recurring-with-syms syms))
                ))
     )))
